5d68296d150636619c766b6e3ee53ff78f58bcf6
my9074 <313231068@qq.com>
2017-12-11 18:36:55 +0800

    Fix the bug for the component library import

--- build/config.js
+++ build/config.js
@@ -10,21 +10,21 @@ var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
-  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`;
+  externals[`@jd/element-ui/packages/${key}`] = `@jd/element-ui/lib/${key}`;
 });
 
-externals['element-ui/src/locale'] = 'element-ui/lib/locale';
+externals['@jd/element-ui/src/locale'] = '@jd/element-ui/lib/locale';
 utilsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`;
+  externals[`@jd/element-ui/src/utils/${file}`] = `@jd/element-ui/lib/utils/${file}`;
 });
 mixinsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`;
+  externals[`@jd/element-ui/src/mixins/${file}`] = `@jd/element-ui/lib/mixins/${file}`;
 });
 transitionList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`;
+  externals[`@jd/element-ui/src/transitions/${file}`] = `@jd/element-ui/lib/transitions/${file}`;
 });
 
 externals = [Object.assign({
