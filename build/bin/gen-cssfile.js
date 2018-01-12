3c73206db0861fe89e79983bb613818262f7fe09
my9074 <313231068@qq.com>
2018-01-12 17:24:34 +0800

    Add theme-background style

--- build/bin/gen-cssfile.js
+++ build/bin/gen-cssfile.js
@@ -2,7 +2,8 @@ var fs = require('fs');
 var path = require('path');
 var Components = require('../../components.json');
 var themes = [
-  'theme-default'
+  'theme-default',
+  'theme-background'
 ];
 Components = Object.keys(Components);
 var basepath = path.resolve(__dirname, '../../packages/');
